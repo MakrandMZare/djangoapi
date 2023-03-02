da04765851e562877733fd73534dc51267a87444
MakrandMZare <makzare1463@gmail.com>
2023-03-03 00:09:40 +0530

    Initial commit.

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import re_path
+from EmployeeApp import views
+
+from django.conf import settings
+
+from django.views.static import serve
+
+
+urlpatterns = [
+ re_path(r'^department$',views.departmentApi),
+ re_path(r'^department/([0-9]+)$',views.departmentApi),
+ 
+ 
+ re_path(r'^employee$',views.employeeApi),
+ re_path(r'^employee/([0-9]+)$',views.employeeApi),
+ 
+ re_path(r'^employee/savefile',views.SaveFile)
+]+serve(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT,path=settings.BASE_DIR)
